529cf3238c7aef36c1616ef0a34654d8739a1c8d
noxiousbrainiac <noxiousbrainiac@gmail.com>
2024-02-19 22:06:04 +0600

    - configured Meta icons

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -6,11 +6,13 @@ import android192 from '@/public/android-chrome-192x192.png';
 import android512 from '@/public/android-chrome-512x512.png';
 
 // eslint-disable-next-line import/prefer-default-export
-export const ICONS = [
-  favicon.src,
-  favicon16.src,
-  favicon32.src,
-  appleTouchIcon.src,
-  android192.src,
-  android512.src,
-];
+export const ICONS = {
+  icon: [
+    { url: favicon.src },
+    { url: favicon16.src, sizes: '16x16' },
+    { url: favicon32.src, sizes: '32x32' },
+    { url: android192.src, sizes: '192x192' },
+    { url: android512.src, sizes: '512x512' },
+  ],
+  apple: appleTouchIcon.src,
+};
