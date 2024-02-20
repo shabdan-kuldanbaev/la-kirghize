dae5a94598ca0f7f393ae6e15b06021bfd8f2fc3
noxiousbrainiac <noxiousbrainiac@gmail.com>
2024-02-20 07:39:16 +0600

    - refactored HomePageContainer

--- components/containers/homePageContainer.tsx
+++ components/containers/homePageContainer.tsx
@@ -1,14 +1,33 @@
 'use client';
 
-import usePageQuery from '@/lib/hooks/usePageQuery';
+import { useParams } from 'next/navigation';
+import SectionSelector from '@/components/sectionSelector';
+import useDataQuery from '@/lib/hooks/useDataQuery';
+import { CONTENT_TYPES } from '@/lib/helpers';
+import { PAGE_GROQ } from '@/lib/queries';
+import { Locale } from '@/i18n.config';
+import { ISection } from '@/types/types';
+import Hero from '../sections/hero';
 
 function HomePageContainer() {
-  const { data } = usePageQuery({ slug: 'accueil' });
+  const { lang }: { lang: Locale } = useParams();
+  const { data } = useDataQuery({
+    queryKey: CONTENT_TYPES.page,
+    groq: PAGE_GROQ,
+    params: { slug: 'accueil', lang },
+  });
 
-  return (
-    <div style={{ height: '200vh' }}>
-      {data?.title}
-    </div>
+  return data && (
+    <main className="flex-auto">
+      <Hero
+        title={data.metaTitle}
+        description={data.metaDescription}
+        contentFile={data.contentFile}
+      />
+      {data?.sections && data.sections.map((section: ISection) => (
+        <SectionSelector section={section} />
+      ))}
+    </main>
   );
 }
 
