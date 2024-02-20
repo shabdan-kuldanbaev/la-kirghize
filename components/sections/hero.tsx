8b66826e1fc03f48e6e05803148f91ed5d52a3c1
noxiousbrainiac <noxiousbrainiac@gmail.com>
2024-02-20 07:57:49 +0600

    - styled hero section

--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -12,24 +12,26 @@ function Hero({
   contentFile,
 }: Props) {
   return (
-    <div className="relative grid place-content-center h-[100vh]">
-      {title && (
-        <h1 className="text-gray-95 font-bold text-4xl text-center max-w-lg">
+    <div className="relative z-[1] h-[100vh]">
+      <div className="max-w-xl mx-auto px-4 grid place-content-center gap-2 h-full">
+        {title && (
+        <h1 className=" text-gray-95 font-bold text-3xl text-center sm:text-4xl">
           {title}
         </h1>
-      )}
-      {description && (
-        <p className="text-gray-95 text-lg text-center max-w-lg">
+        )}
+        {description && (
+        <p className="text-gray-95 text-lg text-center">
           {description}
         </p>
-      )}
+        )}
+      </div>
       {contentFile && (
         <video
           muted
           autoPlay
           loop
           controls={false}
-          className="absolute z-[-1] object-cover object-center w-full h-full"
+          className="absolute top-0 z-[-1] object-cover object-center w-full h-full"
         >
           <source src={contentFile} />
         </video>
