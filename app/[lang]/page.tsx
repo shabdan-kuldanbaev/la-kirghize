ac2f747719783a72514ef7f07cb31a3677cd5b78
noxiousbrainiac <noxiousbrainiac@gmail.com>
2024-02-23 03:08:36 +0600

    - testing dynamic import

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,8 +1,10 @@
-import HomePageContainer from '@/components/containers/homePageContainer';
+import dynamic from 'next/dynamic';
 import PrefetchProvider from '@/lib/prefetchProvider';
 import { Locale } from '@/i18n.config';
 import { PAGE_GROQ } from '@/lib/queries';
 import { CONTENT_TYPES } from '@/lib/helpers';
+
+const HomePageContainer = dynamic(() => import('@/components/containers/homePageContainer'));
 
 export default async function Home(
   { params: { lang } }:
