f07e4a5d7731b0cb7fb330a051432258fa3cfc85
noxiousbrainiac <noxiousbrainiac@gmail.com>
2024-02-23 04:28:53 +0600

    - testing app without state managment

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,29 +1,20 @@
-// import HomePageContainer from '@/components/containers/homePageContainer';
-import RenderSection from '@/components/renderSection';
-// import PrefetchProvider from '@/lib/prefetchProvider';
+import HomePageContainer from '@/components/containers/homePageContainer';
+import PrefetchProvider from '@/lib/prefetchProvider';
 import { Locale } from '@/i18n.config';
-import QuerySanityFn from '@/lib/api';
 import { PAGE_GROQ } from '@/lib/queries';
-import { ISection } from '@/types/types';
-// import { PAGE_GROQ } from '@/lib/queries';
-// import { CONTENT_TYPES } from '@/lib/helpers';
+import { CONTENT_TYPES } from '@/lib/helpers';
 
 export default async function Home(
   { params: { lang } }:
   { params: { lang: Locale } },
 ) {
-  const { sections } = await QuerySanityFn(PAGE_GROQ, { slug: 'accueil', lang });
-
-  return sections && sections.map((section: ISection) => (
-    <RenderSection key={section.sectionType} section={section} />
-  ));
-  // return (
-  //   <PrefetchProvider
-  //     queryKey={[CONTENT_TYPES.page]}
-  //     groq={PAGE_GROQ}
-  //     params={{ slug: 'accueil', lang }}
-  //   >
-  //     <HomePageContainer />
-  //   </PrefetchProvider>
-  // );
+  return (
+    <PrefetchProvider
+      queryKey={[CONTENT_TYPES.page]}
+      groq={PAGE_GROQ}
+      params={{ slug: 'accueil', lang }}
+    >
+      <HomePageContainer />
+    </PrefetchProvider>
+  );
 }
