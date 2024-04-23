c540dfb61d165fc25654e935a83c640c3b29a8c4
shabdan.kuldanbaev <shabdan.kuldanbaev@yellow.systems>
2024-04-23 20:21:08 +0600

    refactored logo component

--- components/header/logo.tsx
+++ components/header/logo.tsx
@@ -2,20 +2,45 @@ import cn from 'clsx';
 import Link from 'next/link';
 import Image from 'next/image';
 import { bulgarianBridge } from '@/lib/fonts';
-import logoSvg from '@/public/logo.svg';
+import darkSvg from '@/public/logo.svg';
+import lightSvg from '@/public/logoLight.svg';
 
-function Logo({ className } : { className?: string }) {
+type Theme = 'dark' | 'light';
+
+const themes = {
+  dark: darkSvg.src,
+  light: lightSvg.src,
+};
+
+function Logo({
+  className, theme = 'dark',
+} : {
+  className?: string,
+  theme?: Theme
+}) {
   return (
     <Link
       href="/"
       className={cn(
-        'flex items-center gap-3 transition-all ease-in-out duration-300 text-2xl lg:text-4xl active:scale-110',
+        `
+        flex 
+        items-center 
+        gap-3 
+        transition-all 
+        ease-in-out 
+        duration-300 
+        text-2xl 
+        whitespace-nowrap
+        lg:text-4xl 
+        active:scale-110
+        `,
         bulgarianBridge.className,
+        theme === 'dark' ? 'text-black' : 'text-white',
         className,
       )}
     >
       <Image
-        src={logoSvg.src}
+        src={themes[theme]}
         alt="La Kirghize"
         width={48}
         height={48}
