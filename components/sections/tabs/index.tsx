bf89d1c1c1867310d039db46cd962d2de167489d
shabdan.kuldanbaev <shabdan.kuldanbaev@yellow.systems>
2024-04-25 05:07:29 +0600

    - removed console.log

--- components/sections/tabs/index.tsx
+++ components/sections/tabs/index.tsx
@@ -5,7 +5,6 @@ import TextBlock from '@/components/text';
 import { ISection } from '@/types/types';
 
 function TabsSection({ contentList }: ISection) {
-  console.log(contentList);
   const tabs: Array<Tab> = (contentList || []).map((tab) => ({
     title: tab.title,
     value: tab._id,
