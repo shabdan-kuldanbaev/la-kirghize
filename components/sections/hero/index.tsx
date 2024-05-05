0ffc27091f75faf3c9d0843c8df2f9eacdea626f
shabdan.kuldanbaev <shabdan.kuldanbaev@yellow.systems>
2024-05-05 21:04:16 +0600

    - memoized Hero section

--- components/sections/hero/index.tsx
+++ components/sections/hero/index.tsx
@@ -1,49 +1,46 @@
 'use client';
 
-import React from 'react';
+import React, { memo } from 'react';
+
 import TextGenerateEffect from '@/components/ui/text-generate-effect';
+
+import { ISection } from '@/types/types';
 import styles from './styles.module.css';
 
-interface Props {
-  title: string,
-  description?: string,
-  contentFile?: string,
-}
+type Props = Pick<ISection, 'heading' | 'contentFile' | 'description'>;
 
-function Hero({
-  title,
+const Hero = memo(({
+  heading,
   description,
   contentFile,
-}: Props) {
-  return (
-    <section className="relative w-full h-[100vh] bg-cover bg-no-repeat bg-center snap-start">
-      {contentFile && (
-        <video
-          muted
-          autoPlay
-          loop
-          playsInline
-          controls={false}
-          src={contentFile}
-          className={styles.video}
-        />
-      )}
-      <div className={styles.overlay}>
-        <div className="absolute top-0 left-0 right-0 bottom-0 grid place-content-center gap-2 px-4 h-full">
-          {title && (
+}: Props): React.JSX.Element => (
+  <section className="relative w-full h-[100vh] bg-cover bg-no-repeat bg-center snap-start">
+    {contentFile && (
+    <video
+      autoPlay
+      muted
+      loop
+      playsInline
+      controls={false}
+      className={styles.video}
+      src={contentFile}
+    />
+    )}
+    <div className={styles.overlay}>
+      <div className="absolute top-0 left-0 right-0 bottom-0 grid place-content-center gap-2 px-4 h-full">
+        {heading && (
           <h1 className="max-w-xl text-white font-bold text-3xl text-center md:text-5xl">
-            <TextGenerateEffect words={title} />
+            <TextGenerateEffect words={heading} />
           </h1>
-          )}
-          {description && (
+        )}
+        {description && (
           <p className="max-w-xl text-gray-95 text-lg text-center">
             <TextGenerateEffect words={description} />
           </p>
-          )}
-        </div>
+        )}
       </div>
-    </section>
-  );
-}
+    </div>
+  </section>
+));
 
 export default Hero;
