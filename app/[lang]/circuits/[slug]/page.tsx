0c4ea343bc50fc985be014e1b34f405a7cc4bcb5
shabdan.kuldanbaev <shabdan.kuldanbaev@yellow.systems>
2024-05-05 20:28:33 +0600

    - created SlugPage

---
+++ app/[lang]/circuits/[slug]/page.tsx
@@ -0,0 +1,74 @@
+import React, { Suspense } from 'react';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+import RenderSection from '@/components/renderSection';
+import LoadingAnimation from '@/components/ui/loading-animation';
+
+import { sanityFetch } from '@/sanity/lib/fetch';
+import urlForImage from '@/sanity/lib/image';
+import { META_GROQ, PAGE_GROQ } from '@/lib/queries';
+import { ICONS } from '@/lib/metaIcons';
+import { IPage, ISection } from '@/types/types';
+
+type Props = {
+  params: {
+    lang: string;
+    slug: string;
+  };
+};
+
+export async function generateMetadata({ params }: Props): Promise<Metadata | null> {
+  const { lang, slug } = params;
+
+  const page: IPage = await sanityFetch({
+    query: META_GROQ,
+    params: {
+      lang,
+      slug,
+      isTour: false,
+    },
+  });
+
+  if (!page) notFound();
+
+  const metaImage = page.metaImage && urlForImage(page.metaImage);
+
+  return {
+    metadataBase: new URL(`https://la-kirghize.com/${lang}/tours`),
+    title: page.metaTitle,
+    description: page.metaDescription,
+    keywords: page.keywords,
+    openGraph: {
+      images: [metaImage],
+    },
+    icons: ICONS,
+  };
+}
+
+async function SlugPage({ params }: Props) {
+  const { lang, slug } = params;
+
+  const data: IPage = await sanityFetch({
+    query: PAGE_GROQ,
+    params: {
+      lang,
+      slug,
+      isTour: false,
+    },
+  });
+
+  if (!data) {
+    notFound();
+  }
+
+  return (
+    <Suspense fallback={<LoadingAnimation />}>
+      {data && data.sections.map((section: ISection) => (
+        <RenderSection key={section._id} section={section} />
+      ))}
+    </Suspense>
+  );
+}
+
+export default SlugPage;
